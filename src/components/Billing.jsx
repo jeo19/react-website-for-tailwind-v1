06864b6f551b2277141629b5d7d2b8b06c987683
jeo19 <terrakkk@outlook.com>
2022-09-23 11:22:48 +0800

    feat:Add the Billing component step2

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -15,6 +15,12 @@ const Billing = () => (
       <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
       {/* gradient end */}
     </div>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" /> billing &
+        invoicing
+      </h2>
+    </div>
   </section>
 );
 
